667a68bfc7e23d9290d79127d2ffa599701a5625
IbrahimBess <70774207+IbrahimBess@users.noreply.github.com>
2023-02-21 20:39:43 -0500

    Add files via upload

---
+++ _nuxt/app.config.d6d2bb12.js
@@ -0,0 +1,1 @@
+import{m as u,F as s}from"./entry.e08d0e5b.js";function a(e,n){return u()._useHead(e,n)}const o={};s(o);export{a as u};
---
+++ _nuxt/events.5fbbca20.js
@@ -0,0 +1,1 @@
+import{a as e,o as n,b as t}from"./entry.e08d0e5b.js";const c={};function o(r,s){return n(),t("div",null,"events page")}const _=e(c,[["render",o]]);export{_ as default};
